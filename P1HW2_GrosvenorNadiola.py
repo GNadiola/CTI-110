ad1f7daa54d5aaa904b7c51090862d43e4b96fd8
GNadiola <gnadiola@gmail.com>
2025-04-07 19:31:30 +0000

    P1HW2 Done

---
+++ P1HW2_GrosvenorNadiola.py
@@ -0,0 +1,31 @@
+# Nadiola Grosvenor
+# 04/07/2025
+# P1HW2
+# Travel Plans
+
+#Ask user to enter values
+budget = int(input("Please enter your budget: "))
+destination = str(input("Please enter your travel destination: "))
+gas = int(input("Please enter the amount that will be spent on gas: "))
+accomodation = int(input("Please enter the amount that will be spent on accomodations: "))
+food = int(input("Please enter the amount that will be spent on food: "))
+
+#Calculations
+expenses = gas + accomodation + food
+cost = budget - expenses
+print("\n")
+
+#Output
+print("--------Travel Expenses--------")
+print("\n")
+
+print("Location: ", destination)
+print("Initial Budget: ", budget)
+print("\n")
+
+print("Fuel: ", gas)
+print("Accomodation: ", accomodation)
+print("Food: ", food)
+print("\n")
+
+print("Remaining Balance: ", cost)
