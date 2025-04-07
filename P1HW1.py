477c173105ced563811643236fac21c7b0ddd4f4
GNadiola <gnadiola@gmail.com>
2025-04-07 14:53:59 -0400

    Create P1HW1.py

---
+++ P1HW1.py
@@ -0,0 +1,14 @@
+# Nadiola Grosvenor
+# 04/07/2025
+# P1HW1
+# Calculations with functions
+
+print("-----Calculating Exponents-----")
+print("\n")
+
+#Base and Exponent values
+base = int(input("Enter and integer as base value"))
+exponent = int(input("Enter an integer as exponent"))
+
+print("\n")
+print(base, "raised to the power of", exponent, "is", base**exponent, "!" )
