98e72187e7f6102a88b73117be8e8f5d25c9471a
GNadiola <gnadiola@gmail.com>
2025-04-07 14:00:51 -0400

    Add files via upload

---
+++ P1Lab1_GrosvenorNadiola.py
@@ -0,0 +1,9 @@
+#1. Nadiola Grosvenor
+#2. 4/05/25
+#3. P1Lab1
+#4. This assignment teaches the student how to create a Python program using IDLE. It shows how to get input from the user and display output.
+print('Enter your first name:', end=' ')
+first_name = input()
+print('Enter your last name:', end=' ')
+last_name = input()
+print('Hello,', first_name, last_name , '! Welcome to CTI-110!')
