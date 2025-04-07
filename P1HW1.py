0371e425fdd279ab4cafaefb974d82f1f29a520b
GNadiola <gnadiola@gmail.com>
2025-04-07 19:04:39 +0000

    P1HW1 Pt.1 Done

--- P1HW1.py
+++ P1HW1.py
@@ -7,8 +7,14 @@ print("-----Calculating Exponents-----")
 print("\n")
 
 #Base and Exponent values
-base = int(input("Enter and integer as base value"))
-exponent = int(input("Enter an integer as exponent"))
+base = int(input("Enter and integer as base value: "))
+exponent = int(input("Enter an integer as exponent: "))
 
 print("\n")
-print(base, "raised to the power of", exponent, "is", base**exponent, "!" )
+print(base, "raised to the power of", exponent, "is", base**exponent, "!!" )
+
+print("-----Addition and Subtraction-----")
+print("\n")
+
+#User input; inputting integers
+
