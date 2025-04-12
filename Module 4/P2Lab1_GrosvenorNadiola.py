d495fb76b94428acc5ebfa8c7b9a99b48a7fd4e5
GNadiola <gnadiola@gmail.com>
2025-04-12 13:31:54 +0000

    P2Lab1 Start

---
+++ Module 4/P2Lab1_GrosvenorNadiola.py
@@ -0,0 +1,8 @@
+#1. Nadiola Grosvenor
+#2. 4/12/25
+#3. P1Lab1
+#4. This assignment teaches the student how to create a Python program that will calculate the diameter, circumference, and area of a circle
+
+#Inputting the radius
+
+radius = float(input('What is the radius? '))
