da7c97398348b258cc7af271bb2b28f17b624a9b
GNadiola <gnadiola@gmail.com>
2025-04-12 13:38:15 +0000

    P2Lab1 Halfway Done

--- Module 4/P2Lab1_GrosvenorNadiola.py
+++ Module 4/P2Lab1_GrosvenorNadiola.py
@@ -4,5 +4,11 @@
 #4. This assignment teaches the student how to create a Python program that will calculate the diameter, circumference, and area of a circle
 
 #Inputting the radius
-
 radius = float(input('What is the radius? '))
+
+#Calculations
+diameter = 2 * radius
+circumference = 2 * 3.14 * radius
+area = 3.14 * radius * radius
+
+#Outputs
