77c1678dbb8e20ee1db981d06bcc9d3e242c2830
GNadiola <gnadiola@gmail.com>
2025-04-20 20:21:14 +0000

    P3Lab main calculations done

--- Module 5/P3Lab_GrosvenorNadiola.py
+++ Module 5/P3Lab_GrosvenorNadiola.py
@@ -6,4 +6,11 @@
 # Enter the amount
 money = float(input("Please enter the amount of money: $"))
 
+# Calculations and Logic
+dollars = money // 1
+coins = (money * 100) - (dollars * 100)
 
+
+# Output
+print(f'{dollars:.0f}', "Dollars")
+print(f'{coins:.0f}', "Coins")
